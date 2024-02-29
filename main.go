6876091395cb9946790712fb87531de290dcdb74
Revan68 <lukaraspudic@gmail.com>
2024-02-29 12:39:38 +1100

    updated code

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"time"
+
+	"workspace/github.com/Revan68/pokedexcli/internal/pokeapi"
+)
+
 func main() {
-	startRepl()
+	pokeClient := pokeapi.NewClient(5 * time.Second)
+	cfg := &config{
+		pokeapiClient: pokeClient,
+	}
+
+	startRepl(cfg)
 }
